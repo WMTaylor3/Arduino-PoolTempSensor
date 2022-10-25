c74356b7e205c45b3b29b76600b7c9bfbe8a91f0
William Taylor <williammarktaylor@gmail.com>
2022-10-25 23:38:07 +1300

    feat: sketch working and sensor reporting

--- Pool_Temp_Sensor/Pool_Temp_Sensor.ino
+++ Pool_Temp_Sensor/Pool_Temp_Sensor.ino
@@ -1,36 +1,105 @@
+#include <WiFi.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <PubSubClient.h>
+#include "Credentials.h"
 
 // Data wire is plugged into digital pin 2 on the Arduino
 #define ONE_WIRE_BUS 14
 
+// WiFi
+WiFiClient wifiClient;
+// MQTT Client
+PubSubClient mqtt;
 // Setup a oneWire instance to communicate with any OneWire device
 OneWire oneWire(ONE_WIRE_BUS);  
-
 // Pass oneWire reference to DallasTemperature library
 DallasTemperature sensors(&oneWire);
 
-void setup(void)
-{
-  sensors.begin();  // Start up the library
-  Serial.begin(9600);
+
+// Variables
+float currentTemperature = 0.0;
+unsigned long timeOfLastTempUpdate = 0;
+unsigned long tempUpdateInterval = 3600000; // 1 Hour.
+unsigned long timeOfLastReboot = 0;
+unsigned long rebootInterval = 86400000; // 24 Hours.
+static char tempInCharArray[5];
+
+void setup() {
+    Serial.begin(115200);
+  // Start the temperature sensors
+  sensors.begin();
+  
+  // Connect to wifi network
+  connectWifi();
+  
+  // Start the MQTT Client
+  mqtt.setClient(wifiClient);
+  mqtt.setServer(MQTT_SERVER, MQTT_PORT);
+  connectMqtt();
 }
 
-void loop(void)
-{ 
-  // Send the command to get temperatures
-  sensors.requestTemperatures(); 
+void loop(){
+  if(timeHasElapsed(timeOfLastReboot, rebootInterval)) {
+    timeOfLastReboot = millis();
+    ESP.restart();
+  }
 
-  //print the temperature in Celsius
-  Serial.print("Temperature: ");
-  Serial.print(sensors.getTempCByIndex(0));
-  Serial.print((char)176);//shows degrees character
-  Serial.print("C  |  ");
+  if(WiFi.status() != WL_CONNECTED) {
+    connectWifi();
+  }
+
+  if (!mqtt.connected()) {
+    connectMqtt();
+  }
   
-  //print the temperature in Fahrenheit
-  Serial.print((sensors.getTempCByIndex(0) * 9.0) / 5.0 + 32.0);
-  Serial.print((char)176);//shows degrees character
-  Serial.println("F");
+  updateTemperature();
+}
+
+void connectWifi() {
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_NAME);
+  WiFi.begin(WIFI_NAME, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void connectMqtt() {
+  while (!mqtt.connected()) {
+    Serial.println("Connecting to MQTT...");
+    
+    if (mqtt.connect(MQTT_ID, MQTT_USER, MQTT_PASSWORD )) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed with state ");
+      Serial.print(mqtt.state());
+      delay(500);
+    }
+  }
+}
+
+bool timeHasElapsed(unsigned long timeOfLastExe, int delayLength) {
+  return (millis() - timeOfLastExe > delayLength);
+}
+
+void updateTemperature()
+{ 
+  sensors.requestTemperatures();
+  currentTemperature = sensors.getTempCByIndex(0);
   
-  delay(500);
+  if(timeHasElapsed(timeOfLastTempUpdate, tempUpdateInterval)) {
+    dtostrf(currentTemperature, 4, 1, tempInCharArray);
+    Serial.print("Reporting: ");
+    Serial.println(tempInCharArray);
+    mqtt.publish(MQTT_TOPIC, tempInCharArray);
+    timeOfLastTempUpdate = millis();
+  }
 }
